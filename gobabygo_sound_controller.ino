1772c17c033625459f08596e7e1ceff91765a3ce
bhaktaj <33442875+bhaktaj@users.noreply.github.com>
2019-03-24 15:39:50 -0700

    Add files via upload

---
+++ gobabygo_sound_controller.ino
@@ -0,0 +1,231 @@
+/* 
+   Sound Controller for Go Baby Go Conversions
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+   Authors: CJ Evans, Aya Nakayama, Carmen Quispe, and Jayesh Bhakta (bhaktaj@lacitycollege.edu)
+   Institution: Los Angeles City College, Los Angeles, CA 90029
+   Date: 10/27/2018 
+   Version: 1.1
+   
+   
+   
+*/
+#include <SoftwareSerial.h>
+#include "Adafruit_Soundboard.h"
+
+/* Hardware Connections to Adafruit Sound Effect Board */
+#define SFX_TX 2
+#define SFX_RX 3
+#define SFX_RST 4
+#define SFX_ACT 5
+#define YELLOW_BTN 6
+#define RED_BTN 7
+#define GREEN_BTN 8
+#define MUSIC_DISABLE 9
+#define MUSIC_STOP_BTN 10
+
+/*music file names*/
+char MUSIC_FILE_YES[12]= "T0100YESOGG";
+char MUSIC_FILE_NOPE[12]= "T01000NOOGG";
+#define MUSIC_FILE_LAST_NUMBER 7
+
+int last_music_track_played;
+
+/* function declarations */
+void flushInput();
+void play_sound_file(int number);
+void stop_sfx();
+
+
+/*software serial*/
+SoftwareSerial ss = SoftwareSerial(SFX_TX, SFX_RX);
+Adafruit_Soundboard sfx = Adafruit_Soundboard(&ss, NULL, SFX_RST);
+boolean retVal;
+
+void setup() 
+{
+  Serial.begin(115200);
+  Serial.println("Adafruit Sound Board!");
+  int attempt;
+  int volume;
+  
+  /*softwareserial at 9600 baud*/
+  ss.begin(9600);
+  //button inputs
+  
+  pinMode(SFX_ACT,INPUT_PULLUP);
+  pinMode(YELLOW_BTN,INPUT_PULLUP); 
+  pinMode(RED_BTN,INPUT_PULLUP); 
+  pinMode(GREEN_BTN,INPUT_PULLUP); 
+  pinMode(MUSIC_DISABLE,INPUT_PULLUP); 
+  pinMode(MUSIC_STOP_BTN,INPUT_PULLUP); 
+  
+  if (!sfx.reset()) 
+  {
+    Serial.println("Not found");
+    while (1);
+  }
+  Serial.println("SFX board found");
+
+  /*Set volume to max level*/
+  attempt=0;
+  while (attempt<20)
+  { 
+      flushInput();
+      volume=sfx.volUp();
+      if (volume>=204)
+        { 
+          Serial.print("Volume initially set to :");
+          Serial.println(volume);
+          break;
+        }
+      attempt++;  
+  }
+
+  last_music_track_played=0;
+}
+
+
+void loop() 
+{
+    flushInput();
+    
+      /*check for button presses*/
+      if(!digitalRead(YELLOW_BTN))
+      {
+          Serial.println("Yellow Button Pressed");
+          
+          if(!digitalRead(SFX_ACT))
+          {
+            /*music is playing so stop music and play next*/
+            stop_sfx();
+          }  
+          
+          if(last_music_track_played>=MUSIC_FILE_LAST_NUMBER)
+          {  
+            play_sound_file(0);
+            last_music_track_played=0;
+          }
+          else
+          {
+            play_sound_file(last_music_track_played++);
+          }
+      }
+
+      if(!digitalRead(RED_BTN))
+      {
+          Serial.println("Red Button Pressed");
+          
+          if (!digitalRead(SFX_ACT))
+          {
+              stop_sfx();    
+          }
+
+          /*play "no" sound */
+          if (!sfx.playTrack(MUSIC_FILE_NOPE))
+          {
+             Serial.println("Failed to play NO vocals");
+          }
+          else
+          {
+            //wait for the sound to complete
+            delay(1000);  
+          }
+      }
+
+      if(!digitalRead(GREEN_BTN))
+      {
+          Serial.println("Green Button Pressed");
+          
+          if (!digitalRead(SFX_ACT))
+          {
+              stop_sfx();    
+          }
+
+          /*play "yes" sound */
+          if (!sfx.playTrack(MUSIC_FILE_YES))
+          {
+             Serial.println("Failed to play YES vocals");
+          }
+          else
+          {
+            //wait for the sound to complete
+            delay(1000);  
+          }
+      }
+
+      if(digitalRead(MUSIC_STOP_BTN))
+      {
+        Serial.println("Stop Button Pressed");
+        
+        if (!digitalRead(SFX_ACT))
+        {
+            stop_sfx();  
+        }
+      }
+
+    delay(250);
+}
+
+/************************ MENU HELPERS ***************************/
+
+void flushInput() 
+{
+  // Read all available serial input to flush pending data.
+  uint16_t timeoutloop = 0;
+  while (timeoutloop++ < 40) {
+    while(ss.available()) {
+      ss.read();
+      timeoutloop = 0;  // If char was received reset the timer
+    }
+    delay(1);
+  }
+}
+
+void play_sound_file(int number)
+{
+  char filename[12];
+  char filenumber_char;
+  
+  /*check number is valid*/
+  if(number>=0 && number<=9 && !digitalRead(MUSIC_DISABLE))
+  { 
+    flushInput();
+    
+    strcpy(filename,"T00NEXT");
+    filenumber_char=48+number;
+    filename[7]=filenumber_char;
+    filename[8]=79; //"O"
+    filename[9]=71; //"G"
+    filename[10]=71; // "G"
+    filename[11]=0; // NULL
+    if(! sfx.playTrack(filename))
+    {
+      Serial.println("Failed to play track");
+    }
+    Serial.print("Now Playing: ");
+    Serial.println(filename);
+  }
+}
+
+void stop_sfx()
+{
+    if (!sfx.stop())
+      {
+        Serial.println("Failed to stop device");
+      }  
+}
+
+
---
+++ motor_pwm_with_switch.ino
@@ -0,0 +1,162 @@
+#include <stdint.h>
+/* 
+   Motor speed controller for Go Baby Go Conversions
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+   Authors: CJ Evans, Aya Nakayama, Carmen Quispe, and Jayesh Bhakta (bhaktaj@lacitycollege.edu)
+   Institution: Los Angeles City College, Los Angeles, CA 90029
+   Date: 07/23/2018 
+   
+   
+   
+*/
+   
+
+/*Hardware Configuration*/
+const uint8_t MOSFET_GATE_PIN=6;
+const uint8_t TRIGGER_INPUT=10;
+const uint8_t ADC_BATTERY=2;
+const uint8_t ADC_DRAIN=1;
+const uint8_t LED_RED=4;
+const uint8_t LED_YELLOW=3;
+const uint8_t LED_GREEN=2;
+
+/*Controller alogrithm constants*/
+const int LOOPS_PER_SPEED_INCREMENT=1;
+const int TARGET_BEMF_MIN=100;
+const int MAX_BEMF=900;
+const int PWM_PERIOD_US=2500;
+const int BEMF_SETTLING_TIME_US=500;
+
+/*global variables*/
+volatile int v_batt;
+volatile int v_drain;
+volatile int v_bemf;
+volatile int v_bemf_error;
+volatile int target_bemf;
+volatile int pwm_ontime_offset_us;
+volatile int pwm_ontime_us=0; 
+volatile unsigned int counter=0;
+
+void setup() 
+{
+  /*set pin MOSFET_GATE_PIN to output*/
+  pinMode(MOSFET_GATE_PIN, OUTPUT);
+  
+  /*ensure pin MOSFET_GATE_PIN is low*/
+  digitalWrite(MOSFET_GATE_PIN,LOW);
+
+  /*configure trigger input pin*/
+  pinMode(TRIGGER_INPUT, INPUT);
+
+  /*configure led outputs*/
+  pinMode(LED_RED,OUTPUT);
+  pinMode(LED_YELLOW,OUTPUT);
+  pinMode(LED_GREEN,OUTPUT);
+}
+
+void loop() 
+{
+  
+  /*set battery level indicator LEDs*/
+  v_batt=3*analogRead(ADC_BATTERY);
+  digitalWrite(LED_GREEN,((v_batt>1024*6/5)?HIGH:LOW));
+  digitalWrite(LED_YELLOW,(((v_batt>1024*5/5)&&(v_batt<1024*6/5))?HIGH:LOW));
+  digitalWrite(LED_RED,((v_batt<1024*5/5)?HIGH:LOW));
+  
+  /*reset the speed_setting*/
+  target_bemf=TARGET_BEMF_MIN;
+  counter=0;
+  pwm_ontime_us=0; 
+  
+  /*motor off*/
+  digitalWrite(MOSFET_GATE_PIN,LOW);
+  
+  /*run motor if trigger has been pressed*/
+  while(!digitalRead(TRIGGER_INPUT))
+  {
+      /*turn the battery indicators off*/
+      digitalWrite(LED_RED, LOW);
+      digitalWrite(LED_YELLOW, LOW);
+      digitalWrite(LED_GREEN, LOW);
+      
+      /*switch motor on for the period corresponding to the duty cycle*/
+      digitalWrite(MOSFET_GATE_PIN,HIGH);
+      delayMicroseconds(pwm_ontime_us);
+  
+      /*switch motor off */
+      digitalWrite(MOSFET_GATE_PIN,LOW);
+      
+      /*wait for the Back EMF (BEMF) settling time*/
+      delayMicroseconds(BEMF_SETTLING_TIME_US);
+      
+      /*measure back emf related voltages, sample 3 times to get average*/
+      v_batt=3*analogRead(ADC_BATTERY);
+      v_drain=3*analogRead(ADC_DRAIN);
+
+      /*complete off time under current pwm duty cycle*/
+      delayMicroseconds(PWM_PERIOD_US-pwm_ontime_us-BEMF_SETTLING_TIME_US);
+
+      /*account for noise on v_drain reading*/
+      if (v_drain>v_batt)
+      {
+        v_drain=v_batt;
+      }
+
+      /*calculate bemf*/
+      v_bemf=v_batt-v_drain;
+
+      /*accelerate motor*/
+      if (target_bemf<MAX_BEMF)
+      { 
+        if((counter%LOOPS_PER_SPEED_INCREMENT)==0)
+        {
+          target_bemf++;
+        }       
+        counter++;
+      }
+
+      /*calculate back emf and compare to the target value*/
+      v_bemf_error=v_bemf-target_bemf;
+      
+      /* calculate pwm_on_time_offset_us */
+      if(target_bemf<=200)
+      {
+        pwm_ontime_offset_us=200;
+      }
+      else
+      {
+        pwm_ontime_offset_us=target_bemf;
+      }
+
+      /*calculate the pwm_ontime_us*/
+      if(pwm_ontime_offset_us>=v_bemf_error)
+      {
+        /*simple proportional control*/
+        pwm_ontime_us=pwm_ontime_offset_us-v_bemf_error;
+        
+        /*limit the maximum pwm_ontime*/ 
+        if(pwm_ontime_us>2000)
+        {
+          pwm_ontime_us=2000;
+        }
+      }
+      else
+      {
+        /*motor running too fast*/
+        pwm_ontime_us=0;
+      }
+  }
+}
